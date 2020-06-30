430755a92605b9c74a97a7f8cc7b60bbb781fd2e
googlpasha <prostobux@yandex.ru>
2020-06-30 20:27:57 +0300

    Test commit

---
+++ Assets/PlayerControl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    public Rigidbody2D rb;
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.velocity = new Vector2(-5, rb.velocity.y);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.velocity = new Vector2(5, rb.velocity.y);
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            rb.velocity = new Vector2(0, 5);
+        }
+    }
+}
