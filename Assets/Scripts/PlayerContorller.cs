74b915a7a2240dea98d720e3515dc8b3578dfd52
Mark Tamarov <googlcraftlol@gmail.com>
2020-06-30 17:53:26 +0300

    init

---
+++ Assets/Scripts/PlayerContorller.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerContorller : MonoBehaviour
+{
+    public float speed = 1f;
+    private Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        rb.MovePosition(rb.position + Vector2.right * moveX * speed * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.Space)) 
+        {
+            rb.AddForce(Vector2.up * 1000);
+        }
+        
+    }
+}
