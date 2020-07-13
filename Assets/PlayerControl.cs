e16547cbf48910dc02269123c18ac5bc864d3a50
googlpasha <prostobux@yandex.ru>
2020-07-14 00:06:59 +0300

    Running animation done

--- Assets/PlayerControl.cs
+++ Assets/PlayerControl.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerControl : MonoBehaviour
 {
     public Rigidbody2D rb;
+    public Animator anim;
 
     // Update is called once per frame
     private void Update()
@@ -15,13 +16,23 @@ public class PlayerControl : MonoBehaviour
             rb.velocity = new Vector2(-5, rb.velocity.y);
             //Sprite flipping
             transform.localScale = new Vector2(-1, 1);
+            //Running animation
+            anim.SetBool("running", true);
         }
-        if (Input.GetKey(KeyCode.D))
+        else if (Input.GetKey(KeyCode.D))
         {
             //Go left
             rb.velocity = new Vector2(5, rb.velocity.y);
             //Sprite flipping
             transform.localScale = new Vector2(1, 1);
+            //Running animation
+            anim.SetBool("running", true);
+        }
+
+        else
+        {
+            //Running animation stop
+            anim.SetBool("running", false);
         }
         if (Input.GetKeyDown(KeyCode.W))
         {
