093345ba7d15e269e44fd5cc690c2c08730aff4d
wssxx <wssxxwssxx@gmail.com>
2020-07-16 21:59:44 +0300

    Menu

---
+++ Assets/Menu/Profile.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Profile : MonoBehaviour
+{
+
+    public Text text_profil;
+    public InputField inputField;
+     void Start()
+    {
+        text_profil.text = new_Text.MyText;
+    }
+
+    public void LoadText() {
+        new_Text.MyText = inputField.text;
+    }
+}
